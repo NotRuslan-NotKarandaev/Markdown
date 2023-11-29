beffdad97708bb90c6180cbd898e49087a84e577
Karandaev-Ruslan <karandaevruslan@gmail.com>
2023-11-29 19:14:10 +0700

    Removed extra code from __main__, menu, menu_cmds

--- clinic/pylint cmd.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy nul pylint_log.txt
-for /R %%f in (*.py) do (
-	python -m pylint --rcfile=.pylintrc %%f >> pylint_log.txt
-	echo ///////////////////////////////////////////////////////////////// >> pylint_log.txt
-)
