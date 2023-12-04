ffd366f8d557f134871826b0088aa3d49057b332
Karandaev-Ruslan <karandaevruslan@gmail.com>
2023-12-04 17:10:57 +0700

    Added documentation about commands of the main menu.

--- markdown/menu_cmds.py
+++ markdown/menu_cmds.py
@@ -1,12 +1,10 @@
 """Shows commands for some menu."""
 
 
-def ...(indent_contr):
-    """Shows ... commands."""
+def print_main_cmds(indent_contr):
+    """Shows main commands."""
     indent_contr. \
         print_text('Press "x" to stop program.\n\n'
         "Types of commands and their codes:\n"
         '1 - help;\n'
-        '2 - ;\n'
-        '3 - ;\n'
-        '4 - .')
+        '2 - get formatted text.')
