95dbf9ffe2d2fb337f7ae29eb8aacd54481682c0
Karandaev-Ruslan <karandaevruslan@gmail.com>
2023-12-22 16:56:43 +0700

    fixed menus_cmds module

--- markdown/menus_cmds.py
+++ markdown/menus_cmds.py
@@ -7,5 +7,5 @@ def print_main_cmds(indent_contr):
         print_text('Press "x" to stop program.\n\n'
         "Types of commands and their codes:\n"
         '1 - help;\n'
-        '2 - get f
-        '3 - get formatted string (my).')
+        '2 - get formatted string with **;\n'
+        '3 - get formatted string with *.')
